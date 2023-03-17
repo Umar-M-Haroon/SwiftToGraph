4837f7ba8b319b3898ecf43eb3cc5e91b0a7ffc6
Umar Haroon <umar.m.haroon@gmail.com>
2023-03-17 02:10:49 -0600

    make public

--- Sources/SwiftToGraph/ParserManager.swift
+++ Sources/SwiftToGraph/ParserManager.swift
@@ -8,7 +8,7 @@
 import Foundation
 import GraphKit
 public actor ParserManager {
-    func parseString(string: String) throws -> Graph {
+    public func parseString(string: String) throws -> Graph {
         let parser = SwiftParser()
         return try parser.parse(source: string)
     }
